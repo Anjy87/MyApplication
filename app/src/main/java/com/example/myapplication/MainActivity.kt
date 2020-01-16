bcde3628671c1226989f38755999f746998d3022
Anjy87 <55833635+Anjy87@users.noreply.github.com>
2020-01-16 11:33:19 -0800

    "My 1st commit"

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View): Unit {
+                // Handler code here.
+                val intent = Intent(this@MainActivity, Demo::class.java);
+                startActivity(intent);
+            }
+        })
+
+
+    }
+}
